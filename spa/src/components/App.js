03df9d3a12117749242be4a6f0fd4ce2f5e52f71
Santosh <santosh.poudyal@gmail.com>
2019-01-08 16:10:30 +1100

    finished state based routing, starting location based routing

--- spa/src/components/App.js
+++ spa/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {BrowserRouter, Route} from "react-router-dom";
 import CategorySelectionPage from "./pages/CategorySelectionPage";
 import HomePage from "./pages/HomePage";
 import NewEntryPage from "./pages/NewEntryPage";
@@ -6,33 +7,12 @@ import NewEntryPage from "./pages/NewEntryPage";
 class App extends Component {
     state = {location: "home"}
 
-    getPage() {
-        const {location} = this.state;
-        
-        switch(location) {
-            case "home":
-                return <HomePage/>
-            case "category":
-                return <CategorySelectionPage/>
-            case "entry":
-                return <NewEntryPage/>
-            default: 
-                return null;
-        }
-    }
-
-    onChangeLocation = (location) => {
-        this.setState({location});
-    }
-
     render() {
         const {location} = this.state;
 
         return(
             <div>
-                <button onClick={()=>this.onChangeLocation("category")}>Category</button>
-                <button onClick={()=>this.onChangeLocation("home")}>Home Page</button>
-                <button onClick={()=>this.onChangeLocation("entry")}>NewEntryPage</button>
+                
                 {this.getPage()}
             </div>
         )
